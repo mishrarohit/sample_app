f53399af256193cddb22e052cb06cf9e8e8c226d
Rohit Mishra <rohit@rohitmishra.me>
2012-06-01 12:57:52 +0530

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,5 +35,11 @@ module SessionsHelper
     session[:return_to] = request.fullpath
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in"
+    end
+  end
 
 end
